d1782c0645711e2c894291f510bdc7d2df73bada
Angelo Capozzi <acap613@gmail.com>
2019-11-04 19:28:15 -0500

    got objects called, start with response.title push to card div

--- logic.js
+++ logic.js
@@ -1,12 +1,30 @@
 
+
+
+
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBmhEAC0h8yrrTxT4LBSPwBOwm7yo4GmJA",
+    authDomain: "revolver-ceb06.firebaseapp.com",
+    databaseURL: "https://revolver-ceb06.firebaseio.com",
+    projectId: "revolver-ceb06",
+    storageBucket: "revolver-ceb06.appspot.com",
+    messagingSenderId: "436349848784",
+    appId: "1:436349848784:web:632121ac7c76b2096522d5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
         
-          // Initial array of games
+        //   // Initial array of games
           var games = [];
     
-          // displaygameInfo function re-renders the HTML to display the appropriate content
-          function displaygameInfo() {
-    
-            var game = $(this).attr("data-name");
+        // displaygameInfo function re-renders the HTML to display the appropriate content
+        function displayGameInfo(game) {
+            console.log("game info")
+            // var game = $(this).attr("data-input");
             // var queryURL = " https://api-v3.igdb.com/games/ab8bfac628708992beb4a32f5a01c89c";
             var settings = {
               "async": true,
@@ -76,52 +94,56 @@
               }
             }
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-          
-          }
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+            });
+            
+        }
     
-          // Function for displaying game data
-          function renderButtons() {
+        //   // Function for displaying game data
+        //   function renderButtons() {
     
-            // Deleting the games prior to adding new games
-            // (this is necessary otherwise you will have repeat buttons)
-            $("#buttons-view").empty();
+        //     // Deleting the games prior to adding new games
+        //     // (this is necessary otherwise you will have repeat buttons)
+        //     $("#buttons-view").empty();
     
-            // Looping through the array of games
-            for (var i = 0; i < games.length; i++) {
+        //     // Looping through the array of games
+        //     for (var i = 0; i < games.length; i++) {
     
-              // Then dynamicaly generating buttons for each game in the array
-              // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-              var a = $("<button>");
-              // Adding a class of game-btn to our button
-              a.addClass("game-btn");
-              // Adding a data-attribute
-              a.attr("data-name", games[i]);
-              // Providing the initial button text
-              a.text(games[i]);
-              // Adding the button to the buttons-view div
-              $("#buttons-view").append(a);
-            }
-          }
+        //       // Then dynamicaly generating buttons for each game in the array
+        //       // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        //       var a = $("<button>");
+        //       // Adding a class of game-btn to our button
+        //       a.addClass("game-btn");
+        //       // Adding a data-attribute
+        //       a.attr("data-name", games[i]);
+        //       // Providing the initial button text
+        //       a.text(games[i]);
+        //       // Adding the button to the buttons-view div
+        //       $("#buttons-view").append(a);
+        //     }
+        //   }
     
           // This function handles events where a game button is clicked
           $("#add-game").on("click", function(event) {
             event.preventDefault();
             // This line grabs the input from the textbox
-            var game = $("#game-input").val().trim();
+            
     
             // Adding game from the textbox to our array
             games.push(game);
     
-            // Calling renderButtons which handles the processing of our game array
-            renderButtons();
+           
           });
     
           // Adding a click event listener to all elements with a class of "game-btn"
-          $(document).on("click", ".game-btn", displaygameInfo);
+          $(document).on("click", "#search",  () => {
+            
+              event.preventDefault();
+              var game = $("#game-input").val().trim();
+              displayGameInfo(game);
+              console.log('only this')
+          });
     
-          // Calling the renderButtons function to display the intial buttons
-          renderButtons();
+         
         
