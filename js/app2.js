5e20026ec63937a8a497bfa6feb85d6d9173ad71
Etienne Warneck <etienne.warneck@gmail.com>
2019-11-08 15:00:57 -0500

    Modal-footer, NavBar, Footer image placement

--- js/app2.js
+++ js/app2.js
@@ -1,14 +1,18 @@
-$(document).ready(function () {
 
+// jQuery READY ------------------------------------------------------------------------
+$(function () {
+
+    // MODAL POP-UP -----------------------------------------------------------------
     $("#myModal").modal();
-     $("#loginButton").on('click', function (e) {
-         console.log("button");
-         var newText = $("#userNameInput:text").val();
-         console.log(newText);
-         $("#userName").text("Hello " + newText);
-         console.log("userName input to navbar")
-     });
+    $("#loginButton").on('click', function (e) {
+        console.log("button");
+        var newText = $("#userNameInput:text").val();
+        console.log(newText);
+        $("#userName").text("Hello " + newText);
+        console.log("userName input to navbar")
+    });
 
+    // FIREBASE ------------------------------------------------------------------
     const firebaseConfig = {
         apiKey: "AIzaSyCwcMO8a208i8hRO2bLwSw6hQoIptXPrYE",
         authDomain: "erudite-flag-256023.firebaseapp.com",
@@ -18,15 +22,18 @@ $(document).ready(function () {
         messagingSenderId: "218813116442",
         appId: "1:218813116442:web:b5908560df242e62de61a3"
     };
-    // Initialize Firebase
+    // FIREBASE INIT ------------------------------------------------------------------
     firebase.initializeApp(firebaseConfig);
     var database = firebase.database();
 
+    //ARRAY
     var games = [];
 
-    // displaygameInfo function re-renders the HTML to display the appropriate content
+    // MAIN FUNCTION ---------------------------------------------------------------------
     function displayGameInfo(game) {
         console.log("game info", game)
+
+        // GET Method to acces RAWG (API#01) --------------------------------------------------
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -38,14 +45,17 @@ $(document).ready(function () {
             }
         }
 
+        //jQuery AJAX to retrieve API#01 infos -----------------------------------------------------------------------------------
         $.ajax(settings).done(function (response) {
             console.log('response', response);
-            $("#name").text(JSON.stringify(response.results[0].name));
-            console.log('checking', $("#name")[0].textContent)
-            $("#date").text(JSON.stringify(response.results[0].released));
-            $("#rating").text(JSON.stringify(response.results[0].rating));
-            $("#store").text(JSON.stringify(response.results[0].stores[0].store.name));
-            $("#platform").text(JSON.stringify(response.results[0].platforms[0].platform.name));
+            $(".card-title").text(JSON.stringify(response.results[0].name)); //Name  
+            // console.log('checking', $("#name")[0].textContent)
+            $("#date").text(JSON.stringify(response.results[0].released)); //Date Released
+            $("#rating-rawg").text(JSON.stringify("RAWG's rating: " + response.results[0].rating)); //Rating
+            $("#score-rawg").text(JSON.stringify("RAWG's score: " + response.results[0].score)); //Score
+            $("#store").text(JSON.stringify(response.results[0].stores[0].store.name)); //Store
+            $("#platform").text(JSON.stringify(response.results[0].platforms[0].platform.name)); //Platform
+            // $("#img-RAWG").attr(response.results[0].image); //MISSING IMG
 
             // Creating a div to hold the game
             //   var name = $("#name");
@@ -90,6 +100,7 @@ $(document).ready(function () {
             //   $("#games-view").prepend(gameDiv);
         });
 
+        // GET Method to acces CHICKEN-COOP (API#02) --------------------------------------------------
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -100,10 +111,18 @@ $(document).ready(function () {
                 "x-rapidapi-key": "f0457cbf4fmshcbc3914de65cf34p14fc1cjsn44b4be2f300a"
             }
         }
-
+        //jQuery AJAX to retrieve API#02 Chicken-Coop infos -----------------------------------------------------------------------------------
         $.ajax(settings).done(function (info) {
             console.log(info);
-            $("#rated").text(JSON.stringify(info.result.developer));
+            // $("#rated").text(JSON.stringify(info.result.developer)); 
+            $(".card-developer").text("Developed by: " + info.result.developer); //DEVELOPER
+            $(".card-rating-chicken").text("Chicken-coop's rating: " + info.result.rating); //RATING
+            $(".card-score-chicken").text("Chicken-coop's score: " + info.result.score); //SCORE
+            var url = info.result.image;
+            $(".card-img").attr('src', url); //IMAGE
+            // console.log("image");
+            $(".bg").css("background-image", "url(" + url + ")"); //BG IMAGE
+            // console.log("background-img");
         });
 
     }
