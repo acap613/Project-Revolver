c657412a4e83ca1cead0d5568db170ac9495e5e6
EtienneWarneck <43478740+EtienneWarneck@users.noreply.github.com>
2019-11-09 09:36:50 -0500

    Heartand skull (#22)
    * sign-in pop-up
    
    * Login works, it pritns name next to the Search button
    
    * design+responsive
    
    * design responsive
    
    * search and name responsive
    
    * Modal-footer, NavBar, Footer image placement
    
    * responsive
    
    * Heart button linked to db, skull button reloads page

--- js/app2.js
+++ js/app2.js
@@ -1,34 +1,119 @@
-var games = [];
+// jQuery READY ------------------------------------------------------------------------
+$(function () {
 
-function displaygameInfo() {
-
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://api.rawg.io/api/games?page_size=1&search=" + game + "",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-            "x-rapidapi-key": "f0457cbf4fmshcbc3914de65cf34p14fc1cjsn44b4be2f300a"
-        }
-    }
-
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-
-        //get title and store it in the div
-        var name = response[0].results.name;
-        // $("#card-title").text(name);
-        $("#card-title").textContent = name;
-
-        // Putting the entire game above the previous games
-        // $("#games-view").prepend(gameDiv);
-
+    // MODAL POP-UP -----------------------------------------------------------------
+    $("#myModal").modal();
+    $("#loginButton").on('click', function (e) {
+        var newText = $("#userNameInput:text").val();
+        // console.log(newText);
+        $("#userName").text("Hello " + newText);
+        // console.log("userName input to navbar")
     });
 
+    //ARRAY
+    var games = [];
 
+    // MAIN FUNCTION ---------------------------------------------------------------------
+    function displayGameInfo(game) {
+
+        // GET Method to acces RAWG (API#01) --------------------------------------------------
+        // var settings = {
+        //     "async": true,
+        //     "crossDomain": true,
+        //     "url": "https://api.rawg.io/api/games?page_size=1&search=" + game + "",
+        //     "method": "GET",
+        //     "headers": {
+        //         "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+        //         "x-rapidapi-key": "f0457cbf4fmshcbc3914de65cf34p14fc1cjsn44b4be2f300a"
+        //     }
+        // }
+
+        //jQuery AJAX to retrieve API#01 infos -----------------------------------------------------------------------------------
+        // $.ajax(settings).done(function (response) {
+        //     console.log('response', response);
+        //     $(".card-title").text(JSON.stringify(response.results[0].name)); //Name  
+        //     // console.log('checking', $("#name")[0].textContent)
+        //     $("#date").text(JSON.stringify(response.results[0].released)); //Date Released
+        //     $("#rating-rawg").text(JSON.stringify("RAWG's rating: " + response.results[0].rating)); //Rating
+        //     $("#score-rawg").text(JSON.stringify("RAWG's score: " + response.results[0].score)); //Score
+        //     $("#store").text(JSON.stringify(response.results[0].stores[0].store.name)); //Store
+        //     $("#platform").text(JSON.stringify(response.results[0].platforms[0].platform.name)); //Platform
+        //     // $("#img-RAWG").attr(response.results[0].image); //MISSING IMG
+        // });
+
+        // GET Method to acces CHICKEN-COOP (API#02) --------------------------------------------------
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://chicken-coop.p.rapidapi.com/games/" + game + "",
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "chicken-coop.p.rapidapi.com",
+                "x-rapidapi-key": "f0457cbf4fmshcbc3914de65cf34p14fc1cjsn44b4be2f300a"
+            }
+        }
+        //jQuery AJAX to retrieve API#02 Chicken-Coop infos -----------------------------------------------------------------------------------
+        $.ajax(settings).done(function (info) {
+            console.log(info);
+            $(".card-title").text(info.result.title); //NAME OF GAME
+            $(".card-developer").text("Developed by: " + info.result.developer); //DEVELOPER
+            $(".card-rating-chicken").text("Chicken-coop's rating: " + info.result.rating); //RATING
+            $(".card-score-chicken").text("Chicken-coop's score: " + info.result.score); //SCORE
+            var url = info.result.image;
+            $(".card-img").attr('src', url); //IMAGE
+            $(".bg").css("background-image", "url(" + url + ")"); //BG IMAGE
+        });
+    };
+
+    // Click on skull reloads the page --------------------------------------------------------------------------
+    $(document).on("click", "#buttonSkull", () => {
+        location.reload();
+    });
+
+    // When Search button is clicked ------------------------------------------------------------------------------------
+    $(document).on("click", "#buttonSearch", () => {
+        event.preventDefault(); //prevents pop-up
+        var game = $("#inputSearch");
+        displayGameInfo(game[0].value);
+    });
+}); //End of MAIN FUNCTION
+
+
+// FIREBASE ------------------------------------------------------------------
+const firebaseConfig = {
+    apiKey: "AIzaSyCwcMO8a208i8hRO2bLwSw6hQoIptXPrYE",
+    authDomain: "erudite-flag-256023.firebaseapp.com",
+    databaseURL: "https://erudite-flag-256023.firebaseio.com",
+    projectId: "erudite-flag-256023",
+    storageBucket: "erudite-flag-256023.appspot.com",
+    messagingSenderId: "218813116442",
+    appId: "1:218813116442:web:b5908560df242e62de61a3"
 };
-// $("#buttonSearch").on("click", function () {
-//     // console.log("click");
-//     var nameOfGame = $("#inputSearch").val().trim();
-// });
+
+// FIREBASE INIT ---------------------------------------------------------------------------------------
+firebase.initializeApp(firebaseConfig);
+
+//FIREBASE REF--------------------------------------------------------------------------------------------
+var database = firebase.database();
+
+// on click event ... ---------------------------------------------------------------------------------------
+$("#buttonHeart").on("click", function (event) {
+    event.preventDefault();
+
+    var gameName = $(".card-title").val(); //NAME OF GAME
+    var gameDeveloper = $(".card-developer").val(); //DEVELOPER
+    var gameRating = $(".card-rating-chicken").val(); //RATING
+    var gameScore = $(".card-score-chicken").val(); //SCORE
+
+    //... SEND TO FIREBASE ---------------------------------------------------------------------------------------
+    database.ref().push({
+        name: gameName,
+        developer: gameDeveloper,
+        rating: gameRating,
+        score: gameScore,
+
+    });
+});
+
+
+
